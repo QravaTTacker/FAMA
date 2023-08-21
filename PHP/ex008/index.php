3aaccbcb9d96c7ff92815e0ed250b011aa47289e
QravaTTacker <cristianpessotto8@gmail.com>
2023-08-21 20:31:47 -0300

    Arquivos do 1º, 2º e 3º Semestre

--- PHP/ex008/index.php
+++ PHP/ex008/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use  ex008\models\Person;
 
-$person = new Person('Maria');
+$person = new Person("Maria");
 
 echo $person->getName() . "\n";
 printf("Result: %s", $person->setName('') != 0 ? 'true' : 'false');
